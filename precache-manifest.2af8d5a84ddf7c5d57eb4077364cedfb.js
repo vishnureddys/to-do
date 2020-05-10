fe16891600abda3eeaca4523f6032867d7e54afc
vishnureddys <vishnusreddy007@gmail.com>
2020-05-10 18:35:23 +0530

    Updates

---
+++ precache-manifest.2af8d5a84ddf7c5d57eb4077364cedfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011b6ebb458417ba3fcc9f88a713759c",
+    "url": "/to-do/index.html"
+  },
+  {
+    "revision": "26b4121c81d337486ade",
+    "url": "/to-do/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3fb7262a258be3638db7",
+    "url": "/to-do/static/js/2.5e478159.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do/static/js/2.5e478159.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b4121c81d337486ade",
+    "url": "/to-do/static/js/main.44bc0325.chunk.js"
+  },
+  {
+    "revision": "37211bdaaf328a19c3c4",
+    "url": "/to-do/static/js/runtime-main.fd3e5cc2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do/static/media/logo.5d5d9eef.svg"
+  }
+]);
